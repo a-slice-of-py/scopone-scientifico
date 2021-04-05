d84cf913a0b5dd8fdb64217c5ced5af508651011
Silvio Lugaro <lugaro.silvio@gmail.com>
2021-04-05 20:15:31 +0200

    feat: improve hands tracking + avoid tie + remove bias in hands rotation

--- scopone_scientifico/core.py
+++ scopone_scientifico/core.py
@@ -4,13 +4,24 @@ import itertools
 from dataclasses import dataclass, field
 from typing import List, Optional, Tuple
 from tqdm.notebook import tqdm
-from loguru import logger
-import sys
-logger.remove()
-logger.add(sys.stderr, level="SUCCESS")
+from IPython.display import HTML
 
 RANKS = {1: 'A', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: 'J', 9: 'Q', 10: 'K'}
 SUITS = {'P': '♠', 'F': '♣', 'D': '♢', 'C': '♡'}
+SEP = '<br>'
+
+OUT_FORMAT = 'html'
+
+if OUT_FORMAT == 'html':
+    ibold = '<b>'
+    iemph = '<i>'
+    obold = '</b>'
+    oemph = '</i>'
+elif OUT_FORMAT == 'markdown':
+    ibold = '**'
+    iemph = '_'
+    obold = ibold
+    oemph = iemph
 
 
 @dataclass
@@ -81,26 +92,29 @@ class Player:
         chosen_card = self.choose_card()               
         options = {combo: value for combo, value in _options.items() if value == chosen_card.rank}
         if options:
-            loot = list(list(options.keys())[0]) # migliorare il criterio di scelta...
-            logger.info(f"{self.name} prende {loot} da {table} giocando {chosen_card}.")
+            loot = list(list(options.keys())[0]) # migliorare il criterio di scelta (se table = [2,3,5], giocando un 5 sono obbligato a prendere un 5...)
+            log = f"{self.name} prende {loot} da {table} giocando {chosen_card}."
             table = list(set(table) - set(loot))            
             self.loot.extend(loot + [chosen_card])
             if not table:
-                logger.info("Scopa!")
+                log += f" {ibold}SCOPA!{obold}"
                 self.scope += 1
         else:
+            log = f"{self.name} gioca {chosen_card}."
             table.append(chosen_card)
-        return table
+        return table, log
 
 class Team:
     
     def __init__(self, name1: str, name2: str):
         self.players = [Player(name1), Player(name2)]
         self.score = 0
+        self.logs = []
         
     def reset(self):
         for player in self.players:
             player.reset()
+        self.logs = []
         
     def __len__(self):
         return len(self.players)
@@ -114,7 +128,7 @@ class Team:
     def count_scope(self):
         scope = sum(player.scope for player in self.players)
         if scope:
-            logger.info(f"+{scope} scope")
+            self.logs.append(f"+{scope} scope")
         return scope
     
     def compute_denari(self):
@@ -122,29 +136,29 @@ class Team:
         _denari = len(list(filter(lambda x: x.suit == 'D', self.loot)))
         if _denari >= 5:
             if _denari == 5:
-                logger.info("Denari pari")
+                self.logs.append("Denari pari")
             elif _denari < 10:
                 denari = 1
-                logger.info("+1 denari")
+                self.logs.append("+1 denari")
             if _denari == 10:
                 denari = 1 + 21
-                logger.info("CAPPOTTO!")
+                self.logs.append("CAPPOTTO!")
         return denari
     
     def compute_settebello(self):
         settebello = 0
         if Card(7, 'D') in self.loot:
             settebello = 1
-            logger.info("+1 settebello")
+            self.logs.append("+1 settebello")
         return settebello        
     
     def compute_carte(self):
         carte = 0
         if len(self.loot) > 20:
             carte = 1
-            logger.info("+1 carte")
+            self.logs.append("+1 carte")
         elif len(self.loot) == 20:
-            logger.info("Carte pari") 
+            self.logs.append("Carte pari") 
         return carte        
     
     def compute_primiera(self):
@@ -156,7 +170,7 @@ class Team:
             self.primiera = sum(rules.get(max(suit, key=lambda x: rules.get(x.rank)).rank) for suit in looted_suits)
     
     def compute_score(self):
-        logger.info(self)
+        self.logs.append(f"{iemph}Punti {self.__repr__()}{oemph}")
         self.loot = self.players[0].loot + self.players[1].loot
         scope = self.count_scope()               
         carte = self.compute_carte()
@@ -169,25 +183,19 @@ class Team:
 
 class Hand:
     
-    def __init__(self, teams: Tuple[Team]):
+    def __init__(self, teams: Tuple[Team], rotation: list):
         self.teams = teams
         self.reset()
+        self.rotation = rotation
     
     def reset(self):
         for team in self.teams:
             team.reset()
         self.players = list(player for team in self.teams for player in team)
-        first_player = random.choice(tuple(player for player in self.teams[0]))
-        second_player = random.choice(tuple(player for player in self.teams[1]))
-        self.rotation = (
-            first_player, 
-            second_player, 
-            (set(self.teams[0]) - set([first_player])).pop(),
-            (set(self.teams[1]) - set([second_player])).pop()
-            )
         self.deck = Deck()
         self.table = []
         self.last_looter: Optional[Player] = None
+        self.logs = []
         
     def distribute_cards(self):        
         cards = self.deck.cards
@@ -199,19 +207,23 @@ class Hand:
         self.reset()
         self.distribute_cards()
         for n in range(len(self.players[0].hand)):
-            logger.info(f"Turno {n + 1}:")
+            self.logs.append(f"{iemph}Turno {n + 1}{oemph}") 
             for player in self.rotation:
-                _table = player.play(self.table)                
+                _table, log = player.play(self.table)
+                self.logs.append(log)                
                 if len(_table) < len(self.table):
                     self.last_looter_idx = self.players.index(player)
                 self.table = _table
+            self.logs.append('')
         if self.table:
-            logger.info(f"{self.players[self.last_looter_idx]} pulisce il tavolo {self.table}.")
+            self.logs.append(f"{self.players[self.last_looter_idx]} pulisce il tavolo {self.table}.{SEP}")
             self.players[self.last_looter_idx].loot.extend(self.table)            
             self.table = []
-            
+        
+        self.logs.append(f"{iemph}Prese{oemph}")
         for player in self.players:
-            logger.info(f"{player.name}: {', '.join(card.__repr__() for card in player.loot)}")
+            self.logs.append(f"{player.name} ➜ {player.loot}")
+        self.logs.append('')
 
         scores = {team: team.compute_score() for team in self.teams}
 
@@ -219,31 +231,61 @@ class Hand:
         if self.teams[0].primiera > self.teams[1].primiera:
             scores[self.teams[0]] += 1
             self.teams[0].score += 1
+            self.teams[0].logs.append("+1 primiera")
         elif self.teams[1].primiera > self.teams[0].primiera:
             scores[self.teams[1]] += 1
             self.teams[1].score += 1
+            self.teams[1].logs.append("+1 primiera")
+        else:
+            self.teams[0].logs.append("Primiera pari")
+            self.teams[1].logs.append("Primiera pari")
+
+        for team in self.teams:
+            self.logs.extend(team.logs)
+            self.logs.append('')
             
         return scores
+
+    def show_logs(self):
+        return HTML(SEP.join(self.logs))
             
 class Match:
     
-    def __init__(self, team1: tuple, team2: tuple):
-        self.teams2scores = {Team(*team1): 0, Team(*team2): 0}
+    def __init__(self, team1: Team, team2: Team, seats: list):
+        self.teams2scores = {team1: 0, team2: 0}
         self.goal = 21
         self.scoreboard = pd.DataFrame(columns=[team.__repr__() for team in self.teams2scores])
+        self.winner = None
+        self.set_rotation(seats)
+        self.hands = []
+
+    def set_rotation(self, seats: list):
+        first_player = random.choice([player for team in self.teams2scores.keys() for player in team.players])
+        self.rotation = seats[seats.index(first_player):] + seats[:seats.index(first_player)]
         
     def run(self):
         for n in itertools.count():
-            if all(score < self.goal for score in self.teams2scores.values()):
-                hand = Hand(tuple(self.teams2scores.keys()))
+            still_no_winners = all(score < self.goal for score in self.teams2scores.values())
+            temporary_tie = len(set(self.teams2scores.values())) == 1
+            if still_no_winners or temporary_tie:
+                hand = Hand(tuple(self.teams2scores.keys()), rotation=self.rotation)
                 scores = hand.run()
+                self.hands.append(hand)
                 for team in self.teams2scores:
                     self.teams2scores[team] += scores[team]
                 self.scoreboard.loc[n + 1, :] = [self.teams2scores[team] for team in self.teams2scores]
-                logger.info(self.teams2scores)
+                # shift rotation
+                self.rotation.append(self.rotation.pop(0))
             else:
                 self.scoreboard.index.name = 'hand'
                 break
+
+        self.n_hands = len(self.scoreboard)
+        self.winner = self.scoreboard.max().idxmax()
+
+    def elect_mvp():
+        # per ogni giocatore, calcola frazione di punti totali del team di cui è responsabile
+        pass
                 
 class Tournament:
     
@@ -253,18 +295,31 @@ class Tournament:
         team2: tuple,
         n_match: int = 10
         ):
-        self.team1 = team1
-        self.team2 = team2
+        self.teams = [Team(*team1), Team(*team2)]
         self.n_match = n_match
         self.matches = []
+        self.assign_seats()
+
+    def assign_seats(self):
+        random.shuffle(self.teams)
+        random.shuffle(self.teams[0].players)
+        random.shuffle(self.teams[1].players)
+        self.seats = [self.teams[k].players[h] for h in range(2) for k in range(2)]
         
     def run(self):
         for n in tqdm(range(self.n_match), "Partite: "):
-            match = Match(team1=self.team1, team2=self.team2)
+            match = Match(team1=self.teams[0], team2=self.teams[1], seats=self.seats)
             match.run()
             self.matches.append(match)
         self.scoreboard = pd.DataFrame(
-            data=[len(match.scoreboard) for match in self.matches],
-            columns=['hands'],
+            data=[
+                [
+                    match.n_hands,
+                    match.winner,
+                    int(match.scoreboard.max().loc[self.teams[0].__repr__()]),
+                    int(match.scoreboard.max().loc[self.teams[1].__repr__()])
+                ]
+                for match in self.matches],
+            columns=['hands', 'winner', self.teams[0].__repr__(), self.teams[1].__repr__()],
             index=range(1, self.n_match + 1)
             )
